8d06b10bb7121cd6d2acae15bbdeb1cff81855d1
rmuldawev <rmuldawev@gmail.com>
2023-01-04 16:36:43 +0500

    firstCommit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi quod
+      ducimus odit? Voluptatum, id ipsam atque, aut mollitia deserunt, dicta
+      fugiat consequuntur officia labore quisquam. Soluta earum libero ipsa
+      quisquam.
+    </div>
+  );
+}
